5a48d1a848df7581626d610fd2e0edb7d0a65a02
JunYao <B10815044@gapps.ntust.edu.tw>
2022-12-13 23:22:37 +0800

    KMeans

--- predict.py
+++ predict.py
@@ -6,14 +6,12 @@ from sklearn.cluster import KMeans
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.cluster import DBSCAN
 from sklearn.decomposition import PCA
 import csv
 
-train_data = pd.read_csv("train.csv")
-train_data = train_data.drop("song_id", axis=1)
+train_data = pd.read_csv("train.csv").drop("song_id", axis=1)
 test_data = pd.read_csv("test_3000.csv")
-
-groups = train_data.groupby(train_data.columns[1:].to_list())
 
 processed_train_data = train_data
 #processed_train_data = processed_train_data.drop("Feature 13", axis=1)
@@ -46,37 +44,24 @@ print(pca.explained_variance_)
 print(pca.n_components_)
 X_transformed = pca.transform(X_std)
 
-wcss = []
-for i in range(1,21):
-    kmeans = KMeans(n_clusters=i,init='k-means++',random_state=42)
-    kmeans.fit(X_transformed)
-    wcss.append(kmeans.inertia_)
-    
-plt.figure(figsize=(10,8))
-plt.plot(range(1,21),wcss,marker='o',linestyle='--')
-plt.xlabel('Num of Clusters')
-plt.ylabel('WCSS')
-plt.show()
+kmeans = KMeans(n_clusters=13,init='k-means++',random_state=42)
+kmeans.fit(X_transformed)
 
-# create and fit the k-means model
-# for i in range(7,13):
-#     ans = []
-#     print(i)
-#     gm = GaussianMixture(n_components=i).fit(X_transformed)
-#     centers = gm.means_
-#     pred = gm.predict(X_transformed)
+kmeans.labels_
 
-#     for index, row in test_data.iterrows():
-#         if(pred[row['col_1']]==pred[row['col_2']]):
-#             ans.append([str(index),str(1)])
-#         else:
-#             ans.append([str(index),str(0)])
-            
-            
-#     with open('ans'+str(i)+'.csv', 'w', newline='') as outfile:
-#         writer = csv.writer(outfile)
-#         writer.writerow(['id', 'ans'])
-#         writer.writerows(ans)
+ans = []
+for index, row in test_data.iterrows():
+    if(kmeans.labels_[row['col_1']]==kmeans.labels_[row['col_2']]):
+        print(kmeans.labels_[row['col_1']],kmeans.labels_[row['col_2']])
+        ans.append([str(index),str(1)])
+    else:
+        ans.append([str(index),str(0)])
+        
+        
+with open('ans'+str(0)+'.csv', 'w', newline='') as outfile:
+    writer = csv.writer(outfile)
+    writer.writerow(['id', 'ans'])
+    writer.writerows(ans)
 
 
 # view the cluster labels
